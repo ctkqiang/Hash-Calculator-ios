b1c47716bcc5a063a8beff1178ad8b373e163c3a
John Melody Me <Johnmelodyme@yandex.com>
2020-05-25 08:48:07 +0800

    ..

--- Hash Calculator/ViewController.swift
+++ Hash Calculator/ViewController.swift
@@ -10,11 +10,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var __output: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
 }
-
